eb00771a0e76621544049da408f3fafffdd5613b
Nikolay Nesterov <nikolaynesterov@gmail.com>
2016-10-03 18:40:46 +0300

    Split instantiate tests

--- src/test/java/ru/rambler/android/grid/LineTest.java
+++ src/test/java/ru/rambler/android/grid/LineTest.java
@@ -10,23 +10,36 @@ import static org.junit.Assert.assertEquals;
 public class LineTest {
 
     @Test
-    public void testInstantiate() throws Exception {
+    public void instantiateLeft_setCorrectValues() throws Exception {
         Line left = Line.fromLeft(1, TypedValue.COMPLEX_UNIT_DIP);
+
         assertEquals(Gravity.LEFT, left.getGravity());
         assertEquals(1, left.getMargin());
+    }
 
+    @Test
+    public void instantiateRight_setCorrectValues() throws Exception {
         Line right = Line.fromRight(2, TypedValue.COMPLEX_UNIT_DIP);
+
         assertEquals(Gravity.RIGHT, right.getGravity());
         assertEquals(2, right.getMargin());
+    }
 
+    @Test
+    public void instantiateTop_setCorrectValues() throws Exception {
         Line top = Line.fromTop(3, TypedValue.COMPLEX_UNIT_DIP);
+
         assertEquals(Gravity.TOP, top.getGravity());
         assertEquals(3, top.getMargin());
+    }
 
+    @Test
+    public void instantiateBottom_setCorrectValues() throws Exception {
         Line bottom = Line.fromBottom(4, TypedValue.COMPLEX_UNIT_DIP);
+
         assertEquals(Gravity.BOTTOM, bottom.getGravity());
         assertEquals(4, bottom.getMargin());
-
-
     }
+
+
 }
